699f33ba8c738a3f839b53dc4666da56ed02bb6c
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-09-12 12:53:51 +0200

    chiffres ajouté

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import TheTitle from './Component/TheTitle';
 import BeautifullScreen from './Component/BeautifulScreen';
+import Number from './Component/AmazingNumberButton';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       </header>
       <main>
         <BeautifullScreen/>
+        <Number/>
       </main>
     </div>
   );
