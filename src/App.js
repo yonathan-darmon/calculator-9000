16e68dd26cb52b96a8daf17b27097aa5de59ee2c
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-09-12 11:22:53 +0200

    z

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TheTitle from './Component/TheTitle';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TheTitle/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/TheTitle.jsx
@@ -0,0 +1,4 @@
+function TheTitle(){
+    return <h1>Calculator9000</h1>
+}
+export default TheTitle
