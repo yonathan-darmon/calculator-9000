ed1ed276129d4c1cfc3062d3e29ff065aa4681d3
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-09-14 12:47:39 +0200

    9000

--- src/Component/ItSOverNineThousand.jsx
+++ src/Component/ItSOverNineThousand.jsx
@@ -1,5 +1,5 @@
 export default function ItSOverNineThousand() {
   return (
-    <div className="over">It’s Over 9000 !!!</div>
+    <div className="over"><img src="https://static.wikia.nocookie.net/f7690f73-fff5-4d39-8aac-74b6269d6030" alt=""></img></div>
   )
 }
